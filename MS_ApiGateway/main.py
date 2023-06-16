e3d355f50035181595827b1ab10bd56a431c497e
yeimer lopez <yeimerlopez1216@gmail.com>
2023-06-15 22:47:18 -0500

    base del proyecto con archivo config.json y main con las importaciones iniciales

---
+++ MS_ApiGateway/main.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_cors import CORS
+import json
+from waitress import serve
+import datetime
+import requests
+
